5d48b244005c7ebd0279e4df6bc1d3ea9537b2ba
R.W.Majeed <raphael@majeed.de>
2016-07-13 00:55:25 +0200

    database/datasource now available during testing and jetty:run

--- admin-gui/src/main/java/org/aktin/dwh/admin/log/LogEndpoint.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/log/LogEndpoint.java
@@ -1,10 +1,16 @@
 package org.aktin.dwh.admin.log;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.aktin.dwh.db.Manager;
 
 /**
  * RESTful endpoint for log management. List log entries and delete the log.
@@ -19,5 +25,24 @@ public class LogEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	Response get(){
 		return Response.ok("TODO").build();
+	}
+	
+	@GET
+	@Path("test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response test(){
+		String ret = "OK";
+		try{
+			Connection dbc = Manager.openConnection();
+			dbc.close();
+		}catch( NamingException e ){
+			e.printStackTrace();
+			ret = e.toString();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ret = e.toString();
+		}
+		return Response.ok(ret).build();
 	}
 }
