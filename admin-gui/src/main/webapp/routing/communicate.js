807728ecc116ce2565edefb3e0e4dc39ff868433
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-08-03 10:46:50 +0200

    communication canal

--- admin-gui/src/main/webapp/routing/communicate.js
+++ admin-gui/src/main/webapp/routing/communicate.js
@@ -4,8 +4,18 @@
     commApp.factory('CommServer', ['$q', '$timeout', '$rootScope', '$state', 
     	function ($q, $timeout, $rootScope, $state) {
     		// communication service to the server. 
-			var send2server = function () {
+			var send2server = function (url, dataObj) {
+				// full url bsp http://localhost:8080/aktin/admin/auth/login
+				// login example 
+				// curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"xyz"}' http://localhost:8080/aktin/admin/auth/login
 
+				var baseUrl = "/aktin/admin";
+				var baseLink = "http://localhost:8080";
+
+				var fullUrl = baseLink+baseUrl+url;
+
+				console.log(url, fullUrl, dataObj);
+				return true;
 			}
 
     		return {
@@ -16,15 +26,51 @@
 			    // -edit
     			users : {
     				// login user
-    				auth : function () {
+    				auth : function (data) {
+    					console.log("commserver user auth with ", data);
+    					var url = "/auth/login";
+    					if (!data || !data.username || !data.password) // data obj null or no username or password
+    						return false;
 
+    					// link aktin/admin/auth/login
+    					// {"username":"admin","password":"xyz"}
+    					return send2server(url, {
+	    						username : data.username,
+	    						password : data.password,
+	    					});
+	    			},
+	    			// logout user
+	    			logout : function (data) {
+
+    					console.log("commserver user logout with ", data);
+    					var url = "/auth/logout";
+    					if (!data)
+    						data = {};
+
+    					return send2server(url, {
+	    						username : data.username,
+	    						token : data.token,
+	    					});
 	    			},
 	    			// check the users auth and roles
-	    			check : function () {
-
+	    			check : function (data) {
+    					console.log("commserver user check roles with ", data);
+    					var url = "/auth/check";
+	    				return send2server (url, {
+	    					username : data.username,
+	    					token : data.token,
+	    					requiredRoles : data.requiredRoles,
+	    				});
 	    			},
 	    			// change user
 	    			update : function () {
+    					console.log("commserver user check roles with ", data);
+    					var url = "/auth/check";
+	    				return send2server (url, {
+	    					username : data.username,
+	    					token : data.token,
+	    					requiredRoles : data.requiredRoles,
+	    				});
 
 	    			},
 	    			// list users
@@ -52,6 +98,7 @@
 			    // -edit
 			    // -view
 			    preferences : {
+			    	// aktin/admin/prefs
 			    	list : function () {
 
 			    	},
@@ -62,3 +109,5 @@
     		};
     	}
     ]);
+
+})();
