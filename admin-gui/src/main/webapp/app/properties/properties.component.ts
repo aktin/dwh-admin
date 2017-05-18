a897ecba563ca23cd6053993bcefcf93c6ed4754
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-05-18 15:41:23 +0200

    properties folder

--- admin-gui/src/main/webapp/app/properties/properties.component.ts
+++ admin-gui/src/main/webapp/app/properties/properties.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `<h1>Hello Properties</h1>`,
+  templateUrl: './properties.component.html',
 })
 export class PropertiesComponent  {
 }
