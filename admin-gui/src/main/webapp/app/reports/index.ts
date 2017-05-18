d2f2423dcf289095f7b0c6a4afb64603f91caee9
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-05-18 15:35:42 +0200

    introduction index in subfolders

---
+++ admin-gui/src/main/webapp/app/reports/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Xu on 18.05.2017.
+ */
+
+/**
+ * report class
+ */
+export * from './report';
+
+/**
+ * report services
+ */
+export * from './report.service';
+
+/**
+ * report components
+ */
+export * from './reports.component';
+export * from './report-single-view.component';
+export * from './report-single.component';
---
+++ admin-gui/src/main/webapp/app/users/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Xu on 18.05.2017.
+ */
+
+/**
+ * user class
+ */
+export * from './user';
+
+/**
+ * user services
+ */
+export * from './user.service';
+export * from './user-auth.guard';
+
+/**
+ * user components
+ */
+export * from './users.component';
+export * from './user-login.component';
