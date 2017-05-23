063e6814466e746472123973a4fc67d5f818af55
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-05-23 16:36:43 +0200

    libs

--- admin-gui/src/main/webapp/systemjs.config.js
+++ admin-gui/src/main/webapp/systemjs.config.js
@@ -6,8 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'lib/node/',
-        // 'underscore':                'node_modules/underscore/underscore.js',
+      'npm:': 'lib/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -25,9 +24,11 @@
       '@angular/forms':                     'npm:@angular/forms/forms.umd.js',
 
       // other libraries
-      'underscore':                'npm:underscore/underscore-min.js',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/in-memory-web-api.umd.js',
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'underscore':                'npm:underscore/underscore-min.js',
+      'jquery':                    'npm:jquery-3.2.0.js',
+      'semantic-ui':               'npm:semantic-ui/semantic.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
