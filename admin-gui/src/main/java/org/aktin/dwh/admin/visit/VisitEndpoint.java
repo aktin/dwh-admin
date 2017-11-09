5a29857701461e5c62b70e97409fc9d6afec6983
R.W.Majeed <raphael@majeed.de>
2017-11-09 22:01:56 +0100

    visit endpoint added

---
+++ admin-gui/src/main/java/org/aktin/dwh/admin/visit/VisitEndpoint.java
@@ -0,0 +1,100 @@
+package org.aktin.dwh.admin.visit;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import javax.xml.namespace.QName;
+import javax.xml.transform.TransformerException;
+
+import org.aktin.Preferences;
+import org.aktin.dwh.Anonymizer;
+import org.aktin.dwh.DataExtractor;
+import org.aktin.dwh.PreferenceKey;
+import org.w3c.dom.Document;
+
+@Path("visit")
+public class VisitEndpoint {
+	private static final Logger log = Logger.getLogger(VisitEndpoint.class.getName());
+	public static final QName EAV_ROOT = new QName("http://sekmi.de/histream/ns/eav-data", "eav-data");
+
+	@Inject
+	private DataExtractor extractor;
+	@Inject
+	private Anonymizer anonymizer;
+	@Inject
+	private Preferences prefs;
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("{root}/{ext}")
+	public void asyncGet(
+			@PathParam("root") String visitRoot, 
+			@PathParam("ext") String visitExt, 
+			@QueryParam("xslt") String xslt,
+			@Suspended AsyncResponse response)
+	{
+		final URL res;
+		if( xslt == null ){
+			res = null;
+		}else{
+			res = getClass().getResource(xslt);
+			if( res == null ){
+				String warn = "Unknown xslt resource: "+xslt;
+				log.warning(warn);
+				response.resume(new BadRequestException(warn));
+				return;
+			}
+		}
+		String encounter = anonymizer.calculateEncounterPseudonym(visitRoot, visitExt);
+		log.info("Asynchronous GET delegated");
+		CompletableFuture<Document> doc = extractor.extractEncounterXML(encounter, EAV_ROOT);
+		doc.whenComplete( (d,t) -> {
+			if( t != null ){
+				log.log(Level.WARNING, "Asynchronous GET terminated exceptionally", t);
+				response.resume(t);
+			}else if( d != null ){
+				log.info("Asynchronous GET completed");
+				if( res != null ){
+					// transform
+					VisitTransformer vt = new VisitTransformer(res);
+					try {
+						d = vt.transform(d);
+					} catch (TransformerException | IOException e) {
+						log.log(Level.WARNING, "Visit transformation failed", e);
+						response.resume(e);
+						return;
+					}
+				}
+				response.resume(d);
+			}
+		});
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("{ext}")
+	public void asyncGet(
+			@PathParam("ext") String visitExt, 
+			@QueryParam("xslt") String xslt,
+			@Suspended AsyncResponse response)
+	{
+		String visitRoot = prefs.get(PreferenceKey.cdaEncounterRootPreset);
+		Objects.requireNonNull(visitRoot);
+		asyncGet(visitRoot, visitExt, response);
+	}
+
+}
