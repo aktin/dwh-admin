c3ce0021e331fa16249025d5643e749f9a045e9c
Corinna Feeken <corinna.feeken1@uni-oldenburg.de>
2018-08-31 15:06:06 +0200

    Added expired status (not yet for deleted requests), changed representation of iso date format.

--- admin-gui/src/main/webapp/app/helpers/pipes/duration.pipe.ts
+++ admin-gui/src/main/webapp/app/helpers/pipes/duration.pipe.ts
@@ -5,34 +5,24 @@ export class DurationQueryPipe implements PipeTransform {
     transform(isoFormat: string): string {
         let result = '';
         let splits = this.splitIso(isoFormat);
-        //console.log(splits);
         if (splits.number === 1) {
             splits.unit === 'D' ? result = 'Täglich' : result = 'Wöchentlich';
-       /* } else if (splits.unit === 'W') {
-            if (splits.number === 4) {
-                result += 'Monatlich';
-            } else if (splits.number > 4) {
-                result += 'alle ' + (splits.number / 4) + 'Monate';
-                if (splits.number % 4 !== 0) {
-                    result += ' ' + (splits.number % 4) + ' Wochen';
-                }
-            }*/
         } else if (splits.unit === 'D') {
             if (splits.number === 7) {
                 result += 'Wöchentlich';
-            } else if (splits.number > 7) {
-                result += 'alle ' + (splits.number / 7) + 'Wochen';
-                if (splits.number % 7 !== 0) {
-                    result += ' ' + (splits.number % 7) + ' Tage';
-                }
+            } else if (splits.number % 7 === 0) {
+                result += 'alle ' + splits.number / 7 + ' Wochen';
+            } else {
+                result += 'alle ' + splits.number + ' Tage';
             }
+        } else if (splits.unit === 'M') {
+            result += 'alle ' + splits.number + ' Monate';
         }
       return result;
     }
 
     // P Num D/M for durationQuery
     splitIso(isoFormat: string) {
-        //console.log(isoFormat);
         let splits = {number: 0, unit: ''};
         let addIndex = 0;
         if (isoFormat.substring(0, 2) === 'PT') {
@@ -46,50 +36,51 @@ export class DurationQueryPipe implements PipeTransform {
 
 @Pipe({name: 'durationData'})
 export class DurationDataPipe implements PipeTransform {
-  transform(isoFormat: string): string {
-      //console.log (isoFormat);
-      let result = '';
-      let splits = this.splitIso(isoFormat, false);
-        if (splits.sign === '+') {
-            result += 'nächste';
-        } else {
-            result += 'vorherige';
-        }
-        if (splits.number === '1') {
-            result += 'r';
-        }
-        result += ' ' + splits.number;
+    transform(isoFormat: string): string {
+        let result = '';
+        let splits = this.splitIso(isoFormat, false);
         if (splits.unit === 'D') {
-            result += ' Tag'
-        } else if (splits.unit === 'W') {
-            result += ' Woche';
-            if (splits.number !== '1') {
-                result += 'n';
+            if (splits.number === 7) {
+                result += 'eine Woche';
+            } else if (splits.number === 1) {
+                result += 'ein Tag';
+            } else if (splits.number % 7 === 0) {
+                result += splits.number / 7 + ' Wochen';
+            } else {
+                result += splits.number + ' Tage';
             }
         } else if (splits.unit === 'M') {
-            result += ' Monat';
+            if (splits.number === 1) {
+                result += 'ein Monat';
+            } else {
+               result += splits.number + ' Monate';
+           }
+        } else if (splits.unit === 'W') {
+            if (splits.number === 1) {
+                result += 'eine Woche';
+            } else {
+                result += splits.number + ' Wochen';
+            }
         }
-        if (splits.number !== '1' && (splits.unit === 'D' || splits.unit === 'M')) {
-            result += 'e';
+        if (splits.sign === '+') {
+            result += ' ab Referenzdatum';
+        } else {
+            result += ' vor Referenzdatum';
         }
-
-   return result;
+    return result;
   }
 
-// P -/+ Num D/W/M for durationData
-  splitIso(isoFormat: string, query: boolean) {
-      let splits = {sign: '', number: '', unit: ''};
-      if (isoFormat === 'PT24H') {
-        isoFormat = 'P1D';
-      }
-    if (isoFormat.charAt(1) === '-') {
-        splits.sign = '-';
-    } else {
-        splits.sign = '+';
-        isoFormat = isoFormat.slice(0, 1) + '+' + isoFormat.slice(1);
+    // P -/+ Num D/W/M for durationData
+    splitIso(isoFormat: string, query: boolean) {
+        let splits = {sign: '', number: 0, unit: ''};
+        if (isoFormat.charAt(1) === '-') {
+            splits.sign = '-';
+        } else {
+            splits.sign = '+';
+            isoFormat = isoFormat.slice(0, 1) + '+' + isoFormat.slice(1);
+        }
+        splits.number = Number(isoFormat.substring(2, isoFormat.length - 1));
+        splits.unit = isoFormat.slice(-1);
+        return splits;
     }
-    splits.number = isoFormat.substring(2, isoFormat.length - 1);
-    splits.unit = isoFormat.slice(-1);
-    return splits;
-  }
 }
