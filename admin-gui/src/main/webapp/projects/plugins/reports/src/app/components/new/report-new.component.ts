143d4d91828ea7e2210ae1a872dc56088f36cf64
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2020-03-04 11:59:08 +0100

    webapp, report new

---
+++ admin-gui/src/main/webapp/projects/plugins/reports/src/app/components/new/report-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-gui-report-new',
+  templateUrl: ",/report-new.component.html",
+  styles: []
+})
+export class ReportNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
