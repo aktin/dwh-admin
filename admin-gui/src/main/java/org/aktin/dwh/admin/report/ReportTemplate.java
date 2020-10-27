ababcb37952907e2a444f491441ff9a6d54ded31
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-27 15:24:06 +0100

    fixed jsonB for "no parameter named version found"

--- admin-gui/src/main/java/org/aktin/dwh/admin/report/ReportTemplate.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/report/ReportTemplate.java
@@ -5,12 +5,11 @@ import org.aktin.report.Report;
 class ReportTemplate {
 	public String id;
 	public String description;
+	public String version;
 
-	public String getVersion(){
-		return "1.3";
-	}
 	public ReportTemplate(Report report){
 		this.id = report.getId(); 
 		this.description = report.getDescription();
+		this.version = "1.3";
 	}
 }
