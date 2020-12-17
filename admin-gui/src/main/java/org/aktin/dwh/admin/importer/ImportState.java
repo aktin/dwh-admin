edfe622f6eb20fb71bdfbc8ffab71168a7eb5f8c
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-12-17 18:28:33 +0100

    added "queued" to ImportState

--- admin-gui/src/main/java/org/aktin/dwh/admin/importer/ImportState.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/importer/ImportState.java
@@ -3,10 +3,12 @@ package org.aktin.dwh.admin.importer;
 public enum ImportState {
     upload_successful,
     verifying,
+    verification_queued,
     verification_successful,
     verification_cancelled,
     verification_failed,
-    importing ,
+    importing,
+    importing_queued,
     import_successful,
     import_cancelled,
     import_failed,
