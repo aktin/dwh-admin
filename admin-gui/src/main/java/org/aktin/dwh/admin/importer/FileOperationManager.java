bf17949a57ae20aff7208e836b7b002f9443304b
Alexander Kombeiz <akombeiz@ukaachen.de>
2021-01-20 09:06:28 +0100

    removed unnecessary dependency

--- admin-gui/src/main/java/org/aktin/dwh/admin/importer/FileOperationManager.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/importer/FileOperationManager.java
@@ -16,7 +16,6 @@ import java.util.*;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // TODO COMMENTS + JAVA DOC
