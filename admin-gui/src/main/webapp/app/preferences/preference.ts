8307c8d54d4905dcc1df288aafcb8534c7be94c0
Alexander Kombeiz <akombeiz@ukaachen.de>
2021-04-16 11:04:53 +0200

    broaded table for preferences, added description for email and import keys

--- admin-gui/src/main/webapp/app/preferences/preference.ts
+++ admin-gui/src/main/webapp/app/preferences/preference.ts
@@ -52,6 +52,11 @@ export const predefinedPreferenceCategories: PreferenceCategory[] = [
         description : 'Konfiguration Wildlfy',
     },
     {
+        value: 'mail',
+        name: 'Mail',
+        description: 'Konfiguration E-Mail Server',
+    },
+    {
         value : 'broker',
         name : 'Broker',
         description : 'Einstellungen Broker',
@@ -59,7 +64,7 @@ export const predefinedPreferenceCategories: PreferenceCategory[] = [
     {
         value : '',
         name : 'Sonstige',
-        description : 'nicht zugeordnete Einstellungen',
+        description : 'Sonstige Einstellungen',
     },
 ];
 
@@ -72,7 +77,8 @@ export const predefinedPreferences = {
     'local.cn': 'Name der DWH Instanz',
     'local.tz': 'Zeitzone',
     'local.email': 'Empfänger-E-Mail für Berichte und Meldungen',
-
+    'local.server.url':'Adresse des lokalen Servers',
+    'local.language': 'Benutzersprache',
 
     'report.data.path': 'Ablageort der Berichte',
     'report.temp.path': 'Temporärer Ablageort der Berichte während der Generierung',
@@ -93,18 +99,14 @@ export const predefinedPreferences = {
     'broker.intervals': 'Heartbeat Update Interval (ISO 8601)',
     'broker.keys': 'Broker-Key zur Identifizierung',
 
-
     'db.datasource': 'AKTIN Datasource im Wildfly-Server',
-
 
     'email.session': 'Konfigurationen der AKTIN Email',
     'email.replyto': 'Rückantwort für AKTIN Berichte',
 
-
     'wildfly.management.url': 'URL der Wildfly-Konfiguration',
     'wildfly.management.user': 'Wildfly Nutzer',
     'wildfly.management.password': 'Wildfly Passwort',
-
 
     'i2b2.project': 'i2b2 Projekt ID',
     'i2b2.service.pm': 'i2b2 PM-Service',
@@ -112,6 +114,20 @@ export const predefinedPreferences = {
     'i2b2.datasource.crc': 'i2b2 Datasource',
     'i2b2.lastimport': 'Zeitstempel des letzten Imports',
 
-
     'rscript.binary': 'Link zum R-Script Binary',
+
+    'import.script.path': 'Pfad für abgelgete Importskripte',
+    'import.script.timeout': 'Maximale Verarbeitungszeit von Importskripten',
+    'import.data.path': 'Ablegeort für hochgeladene Dateien',
+
+    'mail.smtp.starttls.enable': 'Verwendung des TLS Protokolls',
+    'mail.smtp.connectiontimeout': 'Maximale Verbindungszeit zum E-Mail-Server',
+    'mail.x.password': 'Passwort des verwendeten Benutzer',
+    'mail.smtp.timeout': 'Maximale Verarbeitungszeit für E-Mail-Versand',
+    'mail.x.replyto': 'Antwortadresse für versendete E-Mails',
+    'mail.smtp.auth': 'Verwendung von Nutzerauthentifizierung',
+    'mail.smtp.host': 'Adresse des E-Mail-Servers',
+    'mail.smtp.port': 'Port des E-Mail-Servers',
+    'mail.user': 'Verwendeter Benutzer',
+    'mail.transport.protocol': 'Verwendetes E-Mail-Protokoll',
 };
