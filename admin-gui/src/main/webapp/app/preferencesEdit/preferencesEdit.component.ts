baebdd271e4b8cacd4674fe78d60ffeedd5e294e
wiliam <wiliamhoy@gmail.com>
2024-12-09 14:47:39 +0100

    Properties Table loaded with input fields for the values

--- admin-gui/src/main/webapp/app/preferencesEdit/preferencesEdit.component.ts
+++ admin-gui/src/main/webapp/app/preferencesEdit/preferencesEdit.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { PreferenceService } from '../preferences/preference.service';
+import {PreferenceCategory} from "../preferences/preference";
 
 @Component({
-    selector: 'orefEditSelector', // Custom HTML tag for the component
+    selector: 'prefEditSelector', // Custom HTML tag for the component
     templateUrl: './preferencesEdit.component.html',
-    styleUrls: ['./preferencesEdit.component.css']
+    styleUrls: ['./preferencesEdit.component.css'],
+
 })
 export class PreferencesEditComponent {
     // Component logic goes here
-    title = 'New Page';
+    title = 'Konfigurationen Anpassen';
+    constructor (private _prefService: PreferenceService) {}
+
+    get preferenceCategories (): PreferenceCategory[] {
+        // console.log( this._prefService.getPreferenceCategories());
+        return this._prefService.getPreferenceCategories();
+    }
+
 }
