9377fe7787fe452193e669dc2cbcec6e7059332f
Simon H. <huening.simon@gmail.com>
2025-04-10 08:50:07 +0200

    Upgrade to Angular 18 (#43)

--- admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
+++ admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
@@ -4,11 +4,21 @@
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
 
 import { ReportTemplate } from './report';
 import { ReportService } from './report.service';
 import {IMyDateModel, IMyOptions} from "gramli-angular-mydatepicker";
 
+========
+import { IMyDateModel, IMyDpOptions } from 'mydatepicker';
+
+import { ReportTemplate } from './report';
+import { ReportService } from './report.service';
+
+import $ = require('jquery');
+require('semantic-ui');
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
 
 @Component({
     templateUrl: './report-new.component.html',
@@ -18,15 +28,24 @@ export class ReportNewComponent {
 
     template: string;
 
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
     fromDateModel: IMyDateModel;
     toDateModel: IMyDateModel;
 
     fromDPOptions: IMyOptions;
     toDPOptions: IMyOptions;
+========
+    fromDateModel: any = { date: this.formulateDate4DP(new Date()) };
+    toDateModel: any = { date: this.formulateDate4DP(new Date()) };
+
+    fromDPOptions: IMyDpOptions;
+    toDPOptions: IMyDpOptions;
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
 
     showErrorNotification: Boolean = false;
     errorNotificationText: string = "";
 
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
     p: number; // page number
     today = new Date();
 
@@ -40,10 +59,31 @@ export class ReportNewComponent {
         // openSelectorOnInputClick: true,
         dateFormat: 'dd. mmm. yyyy',
         disableSince: {year: this.today.getFullYear(), month: this.today.getMonth() + 1, day: this.today.getDate() + 1},
+========
+    defaultDPOptions: IMyDpOptions = {
+        dayLabels: {su: 'So', mo: 'Mo', tu: 'Di', we: 'Mi', th: 'Do', fr: 'Fr', sa: 'Sa'},
+        monthLabels: { 1: 'Jan', 2: 'Feb', 3: 'Mär', 4: 'Apr', 5: 'Mai', 6: 'Jun',
+            7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Okt', 11: 'Nov', 12: 'Dez' },
+        showTodayBtn: false,
+        editableDateField: false,
+        inline: false,
+        openSelectorOnInputClick: true,
+        dateFormat: 'dd. mmm yyyy',
+        disableSince: this.formulateDate4DP(new Date()),
+    };
+
+    private formulateDate4DP (d: Date): any {
+        return {year: d.getFullYear(), month: d.getMonth() + 1, day: d.getDate()};
+    }
+
+    private DP2date (s: any): Date {
+        return new Date(s.year, s.month - 1, s.day);
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
     }
 
 
     constructor(private _reportService: ReportService, private _router: Router) {
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
         // let date = new Date();
         // let to = new Date();
         //
@@ -56,6 +96,31 @@ export class ReportNewComponent {
 
         this.fromDPOptions = this.defaultDPOptions;
         this.toDPOptions = this.defaultDPOptions;
+========
+        let date = new Date();
+        let to = new Date();
+
+        date.setDate(1);
+        to.setDate(date.getDate() - 1);
+        this.toDateModel.date = this.formulateDate4DP(to);
+
+        date.setMonth(date.getMonth() - 1);
+        this.fromDateModel.date = this.formulateDate4DP(date);
+
+        this.fromDPOptions = this.defaultDPOptions;
+        this.toDPOptions = this.defaultDPOptions;
+
+        // this.toDPOptions.disableUntil = this.formulateDate4DP(date);
+    }
+
+    onFromDateChanged(event: IMyDateModel) {
+        this.fromDateModel.date = event.date;
+        // event properties are: event.date, event.jsdate, event.formatted and event.epoc
+    }
+
+    onToDateChanged(event: IMyDateModel) {
+        this.toDateModel.date = event.date;
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
     }
 
     get templates(): ReportTemplate[] {
@@ -79,7 +144,11 @@ export class ReportNewComponent {
     generateReport(): void {
         let from = null;
         try {
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
             from = this.fromDateModel.singleDate.jsDate;
+========
+            from = this.DP2date(this.fromDateModel.date);
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
         } catch(e){
             this.showErrorNotification = true;
             this.errorNotificationText = "Bitte wählen Sie ein valides Startdatum aus!"
@@ -87,7 +156,11 @@ export class ReportNewComponent {
         };
         let to = null;
         try {
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
             to = this.toDateModel.singleDate.jsDate;
+========
+            to = this.DP2date(this.toDateModel.date);
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
         } catch(e){
             this.showErrorNotification = true;
             this.errorNotificationText = "Bitte wählen Sie ein valides Enddatum aus!"
@@ -95,9 +168,15 @@ export class ReportNewComponent {
         };
         to.setDate(to.getDate() + 1);
         if (from >= to) {
+<<<<<<<< HEAD:admin-gui/src/main/webapp/src/app/reports/report-new.component.ts
             // to.setMonth(from.getMonth() + 1);
             // to.setDate(to.getDate() - 1);
             // this.toDateModel.date = to;
+========
+            to.setMonth(from.getMonth() + 1);
+            to.setDate(to.getDate() - 1);
+            this.toDateModel.date = this.formulateDate4DP(to);
+>>>>>>>> c6594eb (Upgrade to Angular 18 (#43)):admin-gui/src/main/webapp/app/reports/report-new.component.ts
             this.showErrorNotification = true;
             this.errorNotificationText = "Bitte wählen Sie eine passende Zeitspanne von mindestens einem Tag aus!"
             return;
