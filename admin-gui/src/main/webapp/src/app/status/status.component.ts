92df025d813351bc4d551bc110ce678e6e4f3108
Alexander Ivanets <sunberry00@proton.me>
2025-08-06 09:55:19 +0200

    Fix bug: sometimes row in table was not shown. I must refactor status code

--- admin-gui/src/main/webapp/src/app/status/status.component.ts
+++ admin-gui/src/main/webapp/src/app/status/status.component.ts
@@ -1,17 +1,22 @@
 /**
  * Created by Xu on 31-May-17.
  */
-import {Component} from '@angular/core';
-import {ImportStatus} from './import-status';
-import {StatusService} from './status.service';
+
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ImportStatus } from './import-status';
+import { StatusService } from './status.service';
 
 @Component({
     templateUrl: './status.component.html',
 })
-export class StatusComponent  {
-    constructor (private _statusService: StatusService) {}
+export class StatusComponent implements OnInit {
+    status$: Observable<ImportStatus>;
 
-    get status (): ImportStatus {
-        return this._statusService.getImportStatus();
+    constructor(private _statusService: StatusService) {}
+
+    ngOnInit(): void {
+        this.status$ = this._statusService.status$;
+        this._statusService.fetchStatus(); // ✅ manually trigger fetch
     }
 }
