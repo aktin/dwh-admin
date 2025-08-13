02b4615d6d54d4e48fc81cf1a1edf1dafa54d721
Simon Hüning <huening.simon@gmail.com>
2025-08-13 15:26:21 +0200

    #79: automate error display after submit

--- admin-gui/src/main/webapp/src/app/helpers/field/error-message.component.ts
+++ admin-gui/src/main/webapp/src/app/helpers/field/error-message.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Optional} from '@angular/core';
+import {ControlContainer, NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'error-message',
-  templateUrl: './error-message.component.html',
-  styleUrl: './error-message.component.css',
-  host: {
-    class: 'error-message',
-  }
+    selector: 'error-message',
+    templateUrl: './error-message.component.html',
+    styleUrl: './error-message.component.css',
+    host: {
+        class: 'error-message',
+    }
 })
 export class ErrorMessageComponent {
+    constructor(@Optional() private frm: NgForm) {
+    }
+
+    private get isSubmitted(): boolean {
+        return !this.frm || this.frm?.submitted;
+    }
+
+    @HostBinding('style.display')
+    public get displayOnSubmit(): string {
+        return this.isSubmitted ? 'inherit' : 'none';
+    }
 }
--- admin-gui/src/main/webapp/src/app/helpers/field/field.component.ts
+++ admin-gui/src/main/webapp/src/app/helpers/field/field.component.ts
@@ -1,29 +1,35 @@
 import {
     Component,
-    ContentChild,
     ContentChildren,
-    Directive,
     ElementRef,
     HostBinding,
-    OnChanges,
+    Optional,
     QueryList,
-    SimpleChanges, ViewEncapsulation
+    ViewEncapsulation
 } from '@angular/core';
 import {ErrorMessageComponent} from './error-message.component';
-import {NgModel} from '@angular/forms';
+import {ControlContainer, NgForm} from '@angular/forms';
 
 @Component({
     selector: '.field',
     styleUrl: './field.component.css',
     templateUrl: "./field.component.html",
+    providers: [{provide: ControlContainer, useExisting: NgForm}],
     encapsulation: ViewEncapsulation.None
 })
 export class FieldComponent {
     @ContentChildren(ErrorMessageComponent, {descendants: true})
     private errorMessageComponents: QueryList<ElementRef>;
 
+    constructor(@Optional() private frm: NgForm) {
+    }
+
+    public get isSubmitted(): boolean {
+        return !this.frm || this.frm?.submitted;
+    }
+
     @HostBinding('class.error')
     private get hasError(): boolean {
-        return !!this.errorMessageComponents?.length;
+        return !!this.errorMessageComponents?.length && this.isSubmitted;
     }
 }
