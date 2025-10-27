72cbc9421384f103bbe6c19087b0aa98b7a6ecbf
akomii <alexkombie@gmail.com>
2025-10-27 11:55:20 +0100

    FIX: replaced @inject of P21StatsSpec with instantiation

--- admin-gui/src/main/java/org/aktin/dwh/admin/importer/ImportScriptStatsEndpoint.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/importer/ImportScriptStatsEndpoint.java
@@ -25,13 +25,10 @@ public class ImportScriptStatsEndpoint {
   @Inject
   private StatsQueryService statsService;
 
-  @Inject
-  private P21StatsSpec p21Spec;
-
   @GET
   @Path("p21")
   public Response p21() {
-    List<Map<String, Object>> stats = statsService.run(p21Spec);
+    List<Map<String, Object>> stats = statsService.run(new P21StatsSpec());
     return Response.ok(stats).build();
   }
 }
