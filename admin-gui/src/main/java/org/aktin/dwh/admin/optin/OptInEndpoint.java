f179b762e424755edb608961e8b4aa2ed8037a36
Simon Hüning <huening.simon@gmail.com>
2026-02-27 11:51:25 +0100

    #89: - change http method

--- admin-gui/src/main/java/org/aktin/dwh/admin/optin/OptInEndpoint.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/optin/OptInEndpoint.java
@@ -8,6 +8,7 @@ import org.aktin.dwh.admin.optin.error.OptInErrorType;
 import org.aktin.dwh.admin.optin.model.PatientEntryRequestDTO;
 import org.aktin.dwh.admin.optin.model.PatientEntryResponseDTO;
 import org.aktin.dwh.admin.optin.validation.CreateOrUpdateGroup;
+import org.aktin.dwh.admin.util.PATCH;
 import org.aktin.dwh.optinout.model.*;
 import org.aktin.dwh.optinout.service.PatientService;
 import org.aktin.dwh.optinout.service.PatientValidator;
@@ -151,19 +152,19 @@ public class OptInEndpoint {
     }
 
     /**
-     * Updates an existing entry
+     * Partly updates an existing entry
      *
      * @param id:    study id
      * @param ref:   type of the patient reference
-     * @param ext:   extension, can be empty
+     * @param ext:   extension
      * @param entry: object that contains further information (participation, sic, comment) about the entry
      * @return updated entry
      */
     @Path("{studyId}/patients/{reference}/{extension}")
-    @PUT
+    @PATCH
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updateEntry(@PathParam("studyId") String id, @PathParam("reference") PatientReference ref,
+    public Response partlyUpdateEntry(@PathParam("studyId") String id, @PathParam("reference") PatientReference ref,
                                 @PathParam("extension") String ext,
                                 @Valid @ConvertGroup(from = Default.class, to = CreateOrUpdateGroup.class) PatientEntryRequestDTO entry) {
         val username = security.getUserPrincipal().getName();
