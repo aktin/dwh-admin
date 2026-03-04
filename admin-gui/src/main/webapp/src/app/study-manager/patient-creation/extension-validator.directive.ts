b5cbda556f2b1035d1ba5fd854a51bbeef03b32a
Simon Hüning <huening.simon@gmail.com>
2026-03-04 17:11:06 +0100

    #89: show message when extension is already present in study

--- admin-gui/src/main/webapp/src/app/study-manager/patient-creation/extension-validator.directive.ts
+++ admin-gui/src/main/webapp/src/app/study-manager/patient-creation/extension-validator.directive.ts
@@ -1,29 +1,44 @@
 import {Directive} from '@angular/core';
-import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
+import {
+    AbstractControl,
+    AsyncValidator,
+    NG_ASYNC_VALIDATORS,
+    NG_VALIDATORS,
+    ValidationErrors,
+    Validator
+} from '@angular/forms';
+import {Observable, of, take} from "rxjs";
+import {StudyManagerService} from "../services/study-manager.service";
+import {PatientValidationService} from "../services/patient-validation.service";
+import {filter, map} from "rxjs/operators";
+import {EntryValidation} from "../models/entry-validation";
 
 @Directive({
     selector: 'input[extension]',
     providers: [
         {
-            provide: NG_VALIDATORS,
+            provide: NG_ASYNC_VALIDATORS,
             useExisting: ExtensionValidatorDirective,
             multi: true,
         },
     ]
 })
-export class ExtensionValidatorDirective implements Validator {
+export class ExtensionValidatorDirective implements AsyncValidator {
     private prefs: {
         separator: string,
         root: string
     } = {separator: '/', root: ''}
 
+    constructor(private validationService: PatientValidationService) {
+    }
+
     /**
      * Validates the given control's value based on specific formatting rules and preferences.
      *
      * @param {AbstractControl} control - The form control to validate. It contains the value to be checked.
-     * @return {ValidationErrors | null} An object containing validation errors if any conditions are violated, or null if the value is valid.
+     * @return {Promise<ValidationErrors> | Observable<ValidationErrors> | null} An observable containing validation errors if any conditions are violated, or null if the value is valid.
      */
-    validate(control: AbstractControl): ValidationErrors | null {
+    validate(control: AbstractControl): Promise<ValidationErrors> | Observable<ValidationErrors>  {
         const value = control.value;
 
         if (!value) {
@@ -32,15 +47,15 @@ export class ExtensionValidatorDirective implements Validator {
 
         // maximal one slash as separator if root is not set in properties
         if (this.prefs['separator'] === '/' && this.prefs['root'] === '' && value.match(/\//g)?.length > 1) {
-            return {slashSep: true};
+            return of({slashSep: true});
         }
         // no slash allowed if root is set in properties (reserved for path syntax)
         if ((this.prefs['separator'] !== '/' || this.prefs['root'] !== '') && value.includes('/')) {
-            return {slash: true};
+            return of({slash: true});
         }
         // separator not as first character if root is not set in properties
         if (this.prefs['root'] === '' && value.slice(0, 1) === this.prefs['separator']) {
-            return {separator: true};
+            return of({separator: true});
         }
         // set correct root and extension by possibly splitting input on separator
         let root = this.prefs['root'];
@@ -58,9 +73,13 @@ export class ExtensionValidatorDirective implements Validator {
         // value of root and extension may not be . or .. (reserved for path syntax)
         const periods = ['.', '..'];
         if (periods.includes(ext) || periods.includes(root)) {
-            return {period: true};
+            return of({period: true});
         }
 
-        return null;
+        return this.validationService.validationData$.pipe(map(v => v?.find(e => e.extension === value)?.validationResults),
+            filter(v => !!v?.length),
+            map(v => v?.find(e => e === EntryValidation.EntryFound)),
+            map(e => !!e ? {'extensionFound': true} : null),
+            take(1));
     }
 }
