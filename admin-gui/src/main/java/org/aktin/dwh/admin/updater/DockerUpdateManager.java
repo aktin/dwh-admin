1c3105ded1d3be995d14846cbe1b0675160e1f4d
WHoy <wiliam.hoy@alumni.fh-aachen.de>
2026-04-27 13:29:42 +0200

    lazy-load-env

--- admin-gui/src/main/java/org/aktin/dwh/admin/updater/DockerUpdateManager.java
+++ admin-gui/src/main/java/org/aktin/dwh/admin/updater/DockerUpdateManager.java
@@ -32,16 +32,10 @@ public class DockerUpdateManager extends AbstractUpdateManager {
     private static final String HOST = "host.docker.internal";
     private static final int APT_UPDATE_PORT = 1004;
     private static final int DWH_UPDATE_PORT = 1005;
-    private static final String COMPOSE_LOCATION = System.getenv("COMPOSE_LOCATION");
 
     @PostConstruct
     public void initialize() {
         LOGGER.log(Level.INFO, "Initializing Docker UpdateManager and triggering APT package list reload...");
-        if (COMPOSE_LOCATION == null || COMPOSE_LOCATION.trim().isEmpty()) {
-            LOGGER.log(Level.WARNING, "COMPOSE_LOCATION is not set or empty. Falling back to an empty socket message.");
-        } else {
-            LOGGER.log(Level.INFO, "COMPOSE_LOCATION found: {0}", COMPOSE_LOCATION);
-        }
         boolean supports = this.supportsCurrentSystem();
         LOGGER.log(Level.INFO, "Supports Current System: " + supports);
         if (supports) {
@@ -71,9 +65,6 @@ public class DockerUpdateManager extends AbstractUpdateManager {
 
     @Override
     public String getMsg() {
-        if (COMPOSE_LOCATION == null) {
-            return "";
-        }
-        return COMPOSE_LOCATION.trim();
+        return System.getenv("COMPOSE_LOCATION").trim();
     }
 }
