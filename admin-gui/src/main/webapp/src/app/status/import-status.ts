b5a9a56bd19c75c3425a0a531f8a61f9ed7b2479
Simon Hüning <huening.simon@gmail.com>
2026-05-04 11:55:29 +0200

    #107: only allow xml (de-)serialization for import status

--- admin-gui/src/main/webapp/src/app/status/import-status.ts
+++ admin-gui/src/main/webapp/src/app/status/import-status.ts
@@ -3,62 +3,71 @@
  */
 
 export interface ImportStatusError {
-    value: string,
     timestamp: Date,
     repeats: number,
     parsedError?: any,
 }
 
+interface XmlResponse {
+    start: string,
+    'last-write': string,
+    'last-reject': string,
+    imported: number,
+    updated: number,
+    invalid: number,
+    failed: number,
+    'last-errors': any[],
+}
+
+
+
 export class ImportStatus {
+    public static parseXml(xml: string): ImportStatus {
+        const root = new DOMParser().parseFromString(xml, "application/xml")?.documentElement;
 
-    public static parseStr(str: string): ImportStatus {
-        if (str) {
-            return ImportStatus.parseObj(JSON.parse(str));
-        } return null;
-    }
+        const text = (selector: string): string => root.querySelector(selector)?.textContent || '';
+        const number = (selector: string): number => parseInt(text(selector), 10) || 0;
 
-    public static parseObj(obj: any): ImportStatus {
+        const obj: XmlResponse = {
+            start: text('start'),
+            'last-write': text('last-write'),
+            'last-reject': text('last-reject'),
+            imported: number('imported'),
+            updated: number('updated'),
+            invalid: number('invalid'),
+            failed: number('failed'),
+            'last-errors': this.parseErrorXmls(root.querySelectorAll('last-errors > *'))
+        };
+
         return new ImportStatus(
             new Date(obj['start']),
-            new Date(obj['lastWrite']),
-            (obj['lastRejectTime']) ? new Date(obj['lastRejectTime']) : null,
-            obj['importedCount'],
-            obj['updatedCount'],
-            obj['invalidCount'],
-            obj['failedCount'],
-            ImportStatus.parseError(obj['lastErrors']),
+            !!obj['last-write'] ? new Date(obj['last-write']) : null,
+            !!obj['last-reject'] ? new Date(obj['last-reject']) : null,
+            obj['imported'],
+            obj['updated'],
+            obj['invalid'],
+            obj['failed'],
+            obj['last-errors'],
         );
     }
 
-    public static parseError(objErr: any[]): ImportStatusError[] {
-        return objErr?.reduce((array, obj) => {
-            let error: any = {};
-            obj['value']?.split('\n')?.forEach((msg: string) => {
-                let ind = msg.indexOf(':');
-                let head = msg.substring(0, ind);
-                if (typeof error[head] === 'undefined') {
-                    error[head] = [];
-                }
-                error[head].push(msg.substring(ind + 1).trim());
-            });
-            array?.push({
-                value : obj['value'],
-                    timestamp : new Date(obj['timestamp']),
-                repeats : obj['repeats'] || 1,
-                parsedError : error,
-            });
-            return array;
-        }, []);
+    private static parseErrorXmls(objErr: NodeListOf<Element>): ImportStatusError[] {
+        return Array.from(objErr)?.map((obj) => ({
+            timestamp: new Date(obj.getAttribute("timestamp")),
+            repeats: !obj.getAttribute("repeats") ? 1 : +obj.getAttribute("repeats"),
+            parsedError: obj.textContent,
+        }));
     }
 
     constructor(
         public start: Date,
         public lastWrite: Date,
-        public lastRejectTime: Date,
-        public importedCount: number,
-        public updatedCount: number,
-        public invalidCount: number,
-        public failedCount: number,
-        public lastErrors: ImportStatusError[],
-    ) {}
+        public lastReject: Date,
+        public imported: number,
+        public updated: number,
+        public invalid: number,
+        public failed: number,
+        public errors: ImportStatusError[],
+    ) {
+    }
 }
