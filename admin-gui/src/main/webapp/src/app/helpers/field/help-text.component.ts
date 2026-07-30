5a1cafb37e21010ca5f8593df03eafe3bc830314
Simon Hüning <huening.simon@gmail.com>
2026-07-30 16:11:45 +0200

    feat: add help-text component

---
+++ admin-gui/src/main/webapp/src/app/helpers/field/help-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'help-text',
+  templateUrl: './help-text.component.html',
+  styleUrl: './help-text.component.scss',
+  host: {
+    class: 'help-text',
+  }
+})
+export class HelpTextComponent {
+}
