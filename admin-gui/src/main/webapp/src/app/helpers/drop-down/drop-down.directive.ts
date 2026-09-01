67249e2f92bf93b8c486416439ac740e7a90eca0
Simon Hüning <huening.simon@gmail.com>
2026-09-01 11:29:30 +0200

    fix: add additional attribute selector to drop-down directive

--- admin-gui/src/main/webapp/src/app/helpers/drop-down/drop-down.directive.ts
+++ admin-gui/src/main/webapp/src/app/helpers/drop-down/drop-down.directive.ts
@@ -3,7 +3,7 @@ import {Directive, ElementRef, OnInit} from '@angular/core';
 declare var $: any;
 
 @Directive({
-  selector: 'drop-down .ui.dropdown',
+  selector: 'drop-down .ui.dropdown, [drop-down] .ui.dropdown',
 })
 export class DropDownDirective implements OnInit {
   constructor(private host: ElementRef) {
